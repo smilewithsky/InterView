c9a648250079cabf11a429797e2967fec5544380
smilewithsky <ruabosolo99@gmail.com>
2023-03-16 14:22:18 +0700

    third

--- src/Components/Question9/Question9_ClassComponents.jsx
+++ src/Components/Question9/Question9_ClassComponents.jsx
@@ -1,37 +1,40 @@
-import React from 'react';
+import React from "react";
 
-class Question9_ClassComponents extends React.Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-            name: ""
-        }
-    }
+class Question9_ClassComponents extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+    };
+  }
 
-    render() {
+  render() {
     const handleSubmit = () => {
-        alert(`Your name is ${this.state.name}`)
-    }
+      alert(`Your name is ${this.state.name}`);
+    };
 
     return (
-        <form onSubmit={() => handleSubmit()}>
-            <div className="form-group">
-                <label>Name: </label>
-                <input value={this.state.name} onChange={(e) => {
-                    this.setState(() => {
-                        return {
-                            ...this.state,
-                            name: e.target.value
-                        }
-                    });
-                }} />
-            </div>
-            <div className="form-group">
-                <input type="submit" />
-            </div>
-        </form>
-    )
-            }
-};  
+      <form onSubmit={() => handleSubmit()}>
+        <div className="form-group">
+          <label>Name: </label>
+          <input
+            value={this.state.name}
+            onChange={(e) => {
+              this.setState(() => {
+                return {
+                  ...this.state,
+                  name: e.target.value,
+                };
+              });
+            }}
+          />
+        </div>
+        <div className="form-group">
+          <input type="submit" />
+        </div>
+      </form>
+    );
+  }
+}
 
-export default Question9_ClassComponents
+export default Question9_ClassComponents;
